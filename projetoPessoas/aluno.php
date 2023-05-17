2964b078a9bb09691d8b04287897b6b1c1b4fb05
jedeias_Luiz <jedeiasluiz3@gmail.com>
2023-05-17 17:56:45 -0300

    Heranças

---
+++ projetoPessoas/aluno.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once ("pessoas.php");
+
+class Aluno extends Pesssoas{
+
+    private int $matr;
+    private $curso;    
+
+	/**
+	 * @return 
+	 */
+	public function getMatr(): int {
+		return $this->matr;
+	}
+	
+	/**
+	 * @param  $matr 
+	 * @return self
+	 */
+	public function setMatr(int $matr): self {
+		$this->matr = $matr;
+		return $this;
+	}
+	
+	/**
+	 * @return mixed
+	 */
+	public function getCurso() {
+		return $this->curso;
+	}
+	
+	/**
+	 * @param mixed $curso 
+	 * @return self
+	 */
+	public function setCurso($curso): self {
+		$this->curso = $curso;
+		return $this;
+	}
+}
+
+?>
---
+++ projetoPessoas/pessoas.php
@@ -0,0 +1,63 @@
+<?php
+
+class Pesssoas{
+
+    private $nome;
+    private $idade;
+    private $sexo;
+
+    public function fazerAniversario() : int
+    {
+        $this->idade ++;
+    }
+
+	/**
+	 * @return mixed
+	 */
+	public function getNome() {
+		return $this->nome;
+	}
+	
+	/**
+	 * @param mixed $nome 
+	 * @return self
+	 */
+	public function setNome($nome): self {
+		$this->nome = $nome;
+		return $this;
+	}
+	
+	/**
+	 * @return mixed
+	 */
+	public function getIdade() {
+		return $this->idade;
+	}
+	
+	/**
+	 * @param mixed $idade 
+	 * @return self
+	 */
+	public function setIdade($idade): self {
+		$this->idade = $idade;
+		return $this;
+	}
+	
+	/**
+	 * @return mixed
+	 */
+	public function getSexo() {
+		return $this->sexo;
+	}
+	
+	/**
+	 * @param mixed $sexo 
+	 * @return self
+	 */
+	public function setSexo($sexo): self {
+		$this->sexo = $sexo;
+		return $this;
+	}
+}
+
+?>
